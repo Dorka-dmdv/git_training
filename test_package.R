12cb12896a0a8ba8427b1b8a738fb299f7634b52
Dorka-dmdv <au777466@uni.au.dk>
2025-09-22 08:39:28 +0000

    second commit

--- test_package.R
+++ test_package.R
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-library(DBI)
-LIBRARY (tidyverse) #Hello world
-=======
 library (DBI)
-library (tidyverse) #Hello world
->>>>>>> cefbada2a33339af9f010699b0851b146dd521c7
+library (tidyverse) #Hello world again
